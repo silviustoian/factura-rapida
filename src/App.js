aec0a4adc283de687086663256a12c264d4bb13f
silviustoian <silviu.stoian96@gmail.com>
2025-06-26 15:59:13 +0300

    test live

--- src/App.js
+++ src/App.js
@@ -376,7 +376,7 @@ function App() {
           disabled={loading}
           className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 disabled:bg-gray-400"
         >
-          Generează Factura Clasică
+          Generează Factura Clasică (test live)
         </button>
       </section>
 
