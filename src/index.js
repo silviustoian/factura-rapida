21b0c710ca4080db5f7f015ee5d9d5c5954f8527
silviustoian <silviu.stoian96@gmail.com>
2025-06-29 17:29:20 +0300

    added key for vercel

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkPubKey}>
